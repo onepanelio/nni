4609a4c2b756e2210444ce65930147d0ff01ceaa
Savan Visalpara <savan77@users.noreply.github.com>
2020-11-09 15:40:20 -0600

    split dataset into train and test set

--- prepare_data.py
+++ prepare_data.py
@@ -2,6 +2,7 @@ import xml.etree.ElementTree as ET
 import os
 import shutil
 import argparse
+import random
 
 def main(args):
 
@@ -10,13 +11,17 @@ def main(args):
 
     # create directories
     for label in root.iter('label'):
-        os.makedirs(os.path.join(args.data_dir, label.find('name').text))
+        os.makedirs(os.path.join(args.data_dir, 'train', label.find('name').text))
+        os.makedirs(os.path.join(args.data_dir, 'test', label.find('name').text))
 
     for img in root.iter('image'):
         #move image
         lbl = img.find('tag').attrib['label']
         if lbl:
-            shutil.move(os.path.join(args.image_dir, img.attrib['name']), os.path.join(args.data_dir, lbl, img.attrib['name']))
+            if random.randrange(100) < args.test_split:
+                shutil.move(os.path.join(args.image_dir, img.attrib['name']), os.path.join(args.data_dir, 'test', lbl, img.attrib['name']))
+            else:
+                shutil.move(os.path.join(args.image_dir, img.attrib['name']), os.path.join(args.data_dir, 'train', lbl, img.attrib['name']))
 
 
 if __name__ == '__main__':
@@ -24,5 +29,6 @@ if __name__ == '__main__':
     parser.add_argument('--xml_path', default='/mnt/data/datasets/annotations/default.xml')
     parser.add_argument('--data_dir', default='/mnt/data/datasets/processed_data')
     parser.add_argument('--image_dir', default='/mnt/data/datasets/images')
+    parser.add_argument('--test_split', default=20, type=int)
     args = parser.parse_args()
     main(args)
