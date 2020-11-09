7688fd281d7e2b5da0b1b209eb9d774171b12718
Savan Visalpara <savan77@users.noreply.github.com>
2020-11-09 05:17:23 -0600

    train model with specific parameters

---
+++ examples/trials/pytorch-classifier/train_model.py
@@ -0,0 +1,167 @@
+"""
+A general purpose classification script using PyTorch.
+"""
+
+import argparse
+import logging
+import nni
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision.datasets import ImageFolder
+import torchvision.models as models
+from torchvision import datasets, transforms
+
+logger = logging.getLogger('pytorch_classifier')
+
+
+# mean = 0.0
+# for images, _ in loader:
+#     batch_samples = images.size(0) 
+#     images = images.view(batch_samples, images.size(1), -1)
+#     mean += images.mean(2).sum(0)
+# mean = mean / len(loader.dataset)
+
+# var = 0.0
+# for images, _ in loader:
+#     batch_samples = images.size(0)
+#     images = images.view(batch_samples, images.size(1), -1)
+#     var += ((images - mean.unsqueeze(1))**2).sum([0,2])
+# std = torch.sqrt(var / (len(loader.dataset)*224*224))
+
+
+def build_model(model_type, num_classes):
+	if model_type == "googlenet":
+		model = models.googlenet(pretrained=True)
+		in_features = 1024
+	elif model_type == "resnet50":
+		model = models.resnet50(pretrained=True)
+		in_features = 2048
+	model.fc = nn.Sequential(nn.Linear(in_features, num_classes),
+                                 nn.LogSoftmax(dim=1))
+	return model
+
+
+def train_one_epoch(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        if (args['batch_num'] is not None) and batch_idx >= args['batch_num']:
+            break
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args['log_interval'] == 0:
+            logger.info('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+
+def test(args, model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            # sum up batch loss
+            test_loss += F.nll_loss(output, target, reduction='sum').item()
+            # get the index of the max log-probability
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    accuracy = 100. * correct / len(test_loader.dataset)
+
+    logger.info('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset), accuracy))
+
+    return accuracy
+
+
+def train(args):
+    use_cuda = not args['no_cuda'] and torch.cuda.is_available()
+
+    torch.manual_seed(args['seed'])
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    train_loader = torch.utils.data.DataLoader(
+       		ImageFolder(root=args['train_dir'], transform=transforms.Compose([
+            	transforms.ToTensor(),
+              # add Normlize with mean and std
+        ])),
+        batch_size=args['batch_size'], shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+            ImageFolder(root=args['test_dir'], transform=transforms.Compose([
+            	transforms.ToTensor(),
+              # add Normlize with mean and std
+        ])),
+        batch_size=args['batch_size'], shuffle=True, **kwargs)
+
+
+    model = build_model(args['model_type'], args['num_classes']).to(device)
+    optimizer = optim.SGD(model.parameters(), lr=args['lr'],
+                          momentum=args['momentum'])
+
+    for epoch in range(1, args['epochs'] + 1):
+        train_one_epoch(args, model, device, train_loader, optimizer, epoch)
+        test_acc = test(args, model, device, test_loader)
+
+        # report intermediate result
+        nni.report_intermediate_result(test_acc)
+        logger.debug('test accuracy %g', test_acc)
+        logger.debug('Pipe send intermediate result done.')
+
+    # report final result
+    nni.report_final_result(test_acc)
+    logger.debug('Final result is %g', test_acc)
+    logger.debug('Send final result done.')
+
+
+def get_params():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch Classification Example')
+    parser.add_argument("--train_dir", type=str,
+                        default='/home/savan/Documents/train_data', help="train data directory")
+    parser.add_argument("--test_dir", type=str,
+                        default='/home/savan/Documents/test_data', help="test data directory")
+    parser.add_argument("--model_type", type=str,
+                        default='googlenet', help="model to train")
+    parser.add_argument('--batch_size', type=int, default=1, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument("--batch_num", type=int, default=None)
+    parser.add_argument("--num_classes", type=int, default=2, metavar='N',
+                        help='number of classes in the dataset')
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                        help='learning rate (default: 0.01)')
+    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                        help='SGD momentum (default: 0.5)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--no_cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--log_interval', type=int, default=1000, metavar='N',
+                        help='how many batches to wait before logging training status')
+
+    args, _ = parser.parse_known_args()
+    return args
+
+
+if __name__ == '__main__':
+    try:
+        params = get_params()
+        print("Current Parameters:\n")
+        print(params)
+        train(params)
+    except Exception as exception:
+        logger.exception(exception)
+        raise
