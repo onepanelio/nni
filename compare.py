fd00e11e66dad037a1c017eef67ab4c0b9b41ba8
savan <vsavan7@gmail.com>
2020-11-10 17:33:16 -0600

    add model comparison script

---
+++ compare.py
@@ -0,0 +1,18 @@
+import json
+
+accuracies = {}
+with open('/tmp/nas-metrics.json') as f:
+    nas = json.load(f)
+
+with open('/tmp/hyperop-metrics.json') as f:
+    hyper = json.load(f)
+
+with open('/tmp/singlemodel-metrics.json') as f:
+    fm = json.load(f)
+
+accuracies['nas_acc'] = [i['value'] for i in nas if i['name'] == 'accuracy'][0]
+accuracies['hyper_acc'] = [i['value'] for i in hyper if i['name'] == 'accuracy'][0]
+accuracies['fm_acc'] = [i['value'] for i in fm if i['name'] == 'accuracy'][0]
+
+max_acc_name = max(accuracies, key=accuracies.get)
+print("Maximum accuracy was {} for {}".format(max(accuracies.values()), max_acc_name))
