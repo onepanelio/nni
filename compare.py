2f6f5c7310906f030146e7c79e750b2587fe9b2d
savan <vsavan7@gmail.com>
2020-11-17 23:45:44 -0600

    convert metrics to float explicitly

--- compare.py
+++ compare.py
@@ -10,9 +10,9 @@ with open('/tmp/hyperop-metrics.json') as f:
 with open('/tmp/singlemodel-metrics.json') as f:
     fm = json.load(f)
 
-accuracies['nas_acc'] = [i['value'] for i in nas if i['name'] == 'accuracy'][0]
-accuracies['hyper_acc'] = [i['value'] for i in hyper if i['name'] == 'accuracy'][0]
-accuracies['fm_acc'] = [i['value'] for i in fm if i['name'] == 'accuracy'][0]
+accuracies['nas_acc'] = [float(i['value']) for i in nas if i['name'] == 'accuracy'][0]
+accuracies['hyper_acc'] = [float(i['value']) for i in hyper if i['name'] == 'accuracy'][0]
+accuracies['fm_acc'] = [float(i['value']) for i in fm if i['name'] == 'accuracy'][0]
 
 max_acc_name = max(accuracies, key=accuracies.get)
 print("Maximum accuracy was {} for {}".format(max(accuracies.values()), max_acc_name))
