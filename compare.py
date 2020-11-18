70517e652d9658c737dc10ea1128bef57a47a300
savan <vsavan7@gmail.com>
2020-11-18 15:23:16 -0600

    add error handling in comparison script

--- compare.py
+++ compare.py
@@ -1,21 +1,30 @@
 import json
 
 accuracies = {}
-with open('/tmp/nas-metrics.json') as f:
-    nas = json.load(f)
-    print("Metrics for Neural Architecture Search: ", nas)
 
-with open('/tmp/hyperop-metrics.json') as f:
-    hyper = json.load(f)
-    print("Metrics for hyper parameter optimization: ", hyper)
+try:
+    with open('/tmp/nas-metrics.json') as f:
+        nas = json.load(f)
+        print("Metrics for Neural Architecture Search: ", nas)
+    accuracies['nas_acc'] = [float(i['value']) for i in nas if i['name'] == 'accuracy'][0]
+except RuntimeError as e:
+    print("Error occurred while reading metrics for NAS: ", e)
 
-with open('/tmp/singlemodel-metrics.json') as f:
-    fm = json.load(f)
-    print("Metrics for model trained with fixed parameters: ", fm)
+try:
+    with open('/tmp/hyperop-metrics.json') as f:
+        hyper = json.load(f)
+        print("Metrics for hyper parameter optimization: ", hyper)
+    accuracies['hyper_acc'] = [float(i['value']) for i in hyper if i['name'] == 'accuracy'][0]
+except RuntimeError as e:
+    print("Error occurred while reading metrics for hyperparameter optimization: ", e)
 
-accuracies['nas_acc'] = [float(i['value']) for i in nas if i['name'] == 'accuracy'][0]
-accuracies['hyper_acc'] = [float(i['value']) for i in hyper if i['name'] == 'accuracy'][0]
-accuracies['fm_acc'] = [float(i['value']) for i in fm if i['name'] == 'accuracy'][0]
+try:
+    with open('/tmp/singlemodel-metrics.json') as f:
+        fm = json.load(f)
+        print("Metrics for model trained with fixed parameters: ", fm)
+    accuracies['fm_acc'] = [float(i['value']) for i in fm if i['name'] == 'accuracy'][0]
+except RuntimeError as e:
+    print("Error occurred while reading metrics for fixed-param model: ", e)
 
 max_acc_name = max(accuracies, key=accuracies.get)
 print("Maximum accuracy was {} for {}".format(max(accuracies.values()), max_acc_name))
